5984a7b87446bcf4cef2cc50c6fb1f019971f5e8
Austin Coose <austin.coose@radian.biz>
2018-05-25 08:33:00 -0600

    Add Subscribe

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
 //
-import logoImg from '../logo.png'
+import logoImg from '../../public/stub-landing.png'
 
 export default withSiteData(() => (
   <div>
