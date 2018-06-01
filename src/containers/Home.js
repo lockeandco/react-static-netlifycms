984f2bb1477d6db146fcc520d439ac737b6dddf8
Austin Coose <austin.coose@radian.biz>
2018-06-01 13:37:42 -0600

    img

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ import { withSiteData } from 'react-static'
 import logoImg from '../../public/landing-image.png'
 
 export default withSiteData(() => (
-  <div style={{ backgroundImage: logoImg}}>
-    
+  <div>
+    <img src={logoImg} alt="" />
   </div>
 ))
