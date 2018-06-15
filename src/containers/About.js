77f8faf1fc1ca928bcdac33df93773f21767f5d2
Austin Coose <austin.coose@radian.biz>
2018-06-15 00:58:48 -0600

    ftest

--- src/containers/About.js
+++ src/containers/About.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default () => (
   <div style={{fontFamily: 'oldGrowth'}}>
-    <h1>This is what we're all about.</h1>
+    <h1 style={{fontFamily: 'oldGrowth'}}>This is what we're all about.</h1>
     <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
   </div>
 )
