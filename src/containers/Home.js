5d262b25ecba0fca01654c118fd9165ef8ee0444
Austin Coose <austin.coose@radian.biz>
2018-06-15 01:58:27 -0600

    extends

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -18,44 +18,45 @@ import {
   Segment,
   Sidebar,
   Visibility,
+  Modal,
 } from 'semantic-ui-react'
 
 import logoImg from '../../public/landing-image.png'
 
 
-export default class HomepageLayoutextends Component {
-  state = { modalOpen: true}
+class HomepageLayout extends Component {
+  state = { modalOpen: true }
 
   handleOpen = () => this.setState({ modalOpen: true })
 
   handleClose = () => this.setState({ modalOpen: false })
 
-  render() {
+  render () {
     return (
-      
-      <Grid container textAlign='center'>
-      <Image src={logoImg} size="massive" />
 
-      <Modal
+      <Grid container textAlign="center">
+        <Image src={logoImg} size="massive" />
+
+        <Modal
         open={this.state.modalOpen}
         onClose={this.handleClose}
         basic
-        size='small'
+        size="small"
         dimmer="blurring"
         closeOnDimmerClick={false}
         closeOnDocumentClick={false}
       >
-        <Header icon='browser' content='Age Verification' />
+        <Header icon="beer" content="Age Verification" />
         <Modal.Content>
           <h3>Are you at least 21 years of age?</h3>
         </Modal.Content>
         <Modal.Actions>
-          <Button color='red' onClick={this.handleClose} inverted>
-            <Icon name='cancel' /> Yes
+          <Button color="red" onClick={this.handleClose} inverted>
+            <Icon name="cancel" /> Yes
           </Button>
-          <Button color='green' onClick={this.handleClose} inverted>
-          <Icon name='checkmark' /> Yes
-        </Button>
+          <Button color="green" onClick={this.handleClose} inverted>
+            <Icon name="checkmark" /> Yes
+          </Button>
         </Modal.Actions>
       </Modal>
       </Grid>
